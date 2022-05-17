1e60cd465d4ed59b5e46a56d8d9b8105d620baa9
James Coffman <jamesc4023@gmail.com>
2022-05-17 15:21:24 -0400

    commit tree 8e7b6c4274cccaa8db1a5eff9af3c48e7706fec7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog
+require 'pry'
 
+class Dog
+  #setter
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  #getter
+  def name 
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = 'Lassie'
+
+binding.pry
